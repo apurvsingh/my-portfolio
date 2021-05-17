f73106717261262e367323e3f3bfda6ee2168b31
Apurv_Singh <Apurv_Singh@Dell.com>
2021-05-17 23:29:43 +0530

    Profile image/card on landing page

---
+++ src/components/avatar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import profilePicture from '../mediaSources/profile-picture.jpg'
+export class Avatar extends Component{
+    render(){
+        return(
+            <div className="proflie-card">
+                <div className="card-header">
+                    <div className="pic">
+                        <img src={profilePicture} alt="Profile Picture"></img>
+                    </div>
+                </div>
+            </div>
+        );
+    };
+}
--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -1,16 +1,22 @@
 import React from 'react';
+import { Avatar } from './avatar';
 import { ParticleBackground } from './particle-background';
 
 export class LandingPage extends React.Component {
     render() {
-      return (<div>
-          <div className="landingPageHeader">
-            <h2 >I'm Apurv</h2>
-              <div id="animatedWhoAmI">
-                <span></span>
-              </div>
+      return (
+      <div>
+        <ParticleBackground/>
+        <div className="landingPageContainer">
+          <Avatar/>
+          
+          <div id="landingPageText" className="landingPageHeader">
+            <h3>I'm Apurv</h3>
+              <span id="animatedWhoAmI"></span>
           </div>
-          <ParticleBackground/>
-      </div>);
+          <button id="enterWebsite" className="top-margin-mini landingPageEnterButton">Enter</button>
+        </div>
+      </div>
+      );
     }
   }
