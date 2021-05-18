fcb4a6de70a9ab5ff29b3797602ca39f7249b00b
Apurv_Singh <Apurv_Singh@Dell.com>
2021-05-19 01:39:40 +0530

    New greySection & myServices component

--- src/components/services.js
+++ src/components/services.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export class Services extends React.Component {
     render(){
-        return(<div id = "services"  className = "test2"></div>);
+        return(<div id = "services"  className = "greySection"></div>);
     }
 }
