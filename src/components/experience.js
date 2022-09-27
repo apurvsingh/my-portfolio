bfbad51b5c502b9fe3598d6e92d9dba511ec1750
apurvsingh <apurvsingh95@outlook.com>
2022-09-26 20:47:36 -0400

    Updating experience section

--- src/components/experience.js
+++ src/components/experience.js
@@ -14,6 +14,15 @@ export class Experience extends React.Component {
 
                   <div className = "experience-box">
                     <IndividualExperienceBox
+                            title = "Engineering Intern"
+                            companyName = "Publicis Sapient, UPS Client"
+                            year = "2022"
+                            description = " 'A-B testing' to drive feature selection for UPS(ups.com) package shipping/tracking flow<br>Bayes' theorem for modeling data<br>Adobe Analytics to build powerful dashboards that provide real-time insights<br>Video analysis to identify the bottlenecks faced by the user resulting in dropouts<br>Typical data lifecycle in a cloud-powered environment."
+                    />
+                  </div>
+
+                  <div className = "experience-box">
+                    <IndividualExperienceBox
                         title = "Software Engineer 2"
                         companyName = "Dell"
                         year = "2018-2021"
@@ -28,14 +37,7 @@ export class Experience extends React.Component {
                             description = "Full stack development to create the b2b checkout flow for a 'Bill Anywhere-Ship Anywhere' global portal system "
                     />
                   </div>
-                  <div className = "experience-box">
-                    <IndividualExperienceBox
-                            title = "Intern"
-                            companyName = "Sapient Nitro"
-                            year = "2016"
-                            description = "UX maintenance using JQuery "
-                    />
-                  </div>
+
                 </div>
               </div>
             </div>
